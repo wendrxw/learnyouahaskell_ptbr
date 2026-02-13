9b3804fdd6272d375c61cf879967bac47e3413ed
Wendrew Oliveira <about.wendrew@gmail.com>
2026-02-13 15:36:34 -0300

    Again... re-build html layout

--- build.hs
+++ build.hs
@@ -21,41 +21,64 @@ outputDir :: FilePath
 outputDir = "docs"
 
 -- Explicitly ordered chapter list
-orderedChapters :: [String]
+orderedChapters :: [(String, String)]
 orderedChapters = 
-    [ "introduction"
-    , "starting-out"
-    , "types-and-typeclasses"
-    , "syntax-in-functions"
-    , "recursion"
-    , "higher-order-functions"
-    , "modules"
-    , "making-our-own-types-and-typeclasses"
-    , "input-and-output"
-    , "functionally-solving-problems"
-    , "functors-applicative-functors-and-monoids"
-    , "a-fistful-of-monads"
-    , "for-a-few-monads-more"
-    , "zippers"
+    [ ("introduction", "Introdução")
+    , ("starting-out", "Começando")
+    , ("types-and-typeclasses", "Tipos e Typeclasses")
+    , ("syntax-in-functions", "Sintaxe em Funções")
+    , ("recursion", "Recursão")
+    , ("higher-order-functions", "Funções de Alta Ordem")
+    , ("modules", "Módulos")
+    , ("making-our-own-types-and-typeclasses", "Criando Nossos Próprios Tipos e Typeclasses")
+    , ("input-and-output", "Entrada e Saída")
+    , ("functionally-solving-problems", "Resolvendo Problemas Funcionalmente")
+    , ("functors-applicative-functors-and-monoids", "Funtores, Funtores Aplicativos e Monoides")
+    , ("a-fistful-of-monads", "Por um Punhado de Monads")
+    , ("for-a-few-monads-more", "Por mais Alguns Monads")
+    , ("zippers", "Zippers")
     ]
+
 
 -- ==========================================
 -- RENDERING LOGIC
 -- ==========================================
 
+-- | Pandoc options with attributes enabled and figures disabled
+readerOpts :: ReaderOptions
+readerOpts = def 
+    { readerExtensions = extensionsFromList 
+        [ Ext_attributes
+        , Ext_link_attributes
+        , Ext_header_attributes
+        , Ext_fenced_code_attributes
+        , Ext_implicit_header_references
+        ] <> (disableExtension Ext_implicit_figures pandocExtensions) 
+    }
+
+writerOpts :: WriterOptions
+writerOpts = def 
+    { writerExtensions = extensionsFromList 
+        [ Ext_attributes
+        , Ext_link_attributes
+        , Ext_header_attributes
+        , Ext_fenced_code_attributes
+        ] <> (disableExtension Ext_implicit_figures pandocExtensions)
+    }
+
 -- | Converts Markdown text to HTML using Pandoc.
 renderMarkdown :: Text -> IO Text
 renderMarkdown input = do
     result <- runIO $ do
-        doc <- readMarkdown def input
-        writeHtml5String def doc
+        doc <- readMarkdown readerOpts input
+        writeHtml5String writerOpts doc
     case result of
         Left err -> return $ "Error parsing markdown: " <> T.pack (show err)
         Right html -> return html
 
 -- | Main layout matching the original Learn You a Haskell design.
-layout :: Maybe String -> Maybe String -> Text -> Text
-layout mPrev mNext content = T.unlines
+layout :: String -> Maybe (String, String) -> Maybe (String, String) -> Text -> Text
+layout bodyClass mPrev mNext content = T.unlines
     [ "<!DOCTYPE html>"
     , "<html lang=\"pt-BR\">"
     , "<head>"
@@ -67,14 +90,14 @@ layout mPrev mNext content = T.unlines
     , "    <link rel=\"stylesheet\" href=\"sh/Styles/SyntaxHighlighter.css\" type=\"text/css\" />"
     , "    <link rel=\"shortcut icon\" href=\"assets/images/favicon.png\" type=\"image/png\" />"
     , "</head>"
-    , "<body>"
+    , "<body class=\"" <> T.pack bodyClass <> "\">"
     , "    <div class=\"bgwrapper\">"
     , "        <div id=\"content\">"
-    ,           renderFootDiv mPrev mNext
+    ,           renderFootDiv True mPrev mNext
     , "            <article>"
     ,               content
     , "            </article>"
-    ,           renderFootDiv mPrev mNext
+    ,           renderFootDiv False mPrev mNext
     , "        </div>"
     , "    </div>"
     , "    <script type=\"text/javascript\" src=\"sh/Scripts/shCore.js\"></script>"
@@ -88,20 +111,46 @@ layout mPrev mNext content = T.unlines
     , "</html>"
     ]
 
-renderFootDiv :: Maybe String -> Maybe String -> Text
-renderFootDiv mPrev mNext = T.unlines
-    [ "<div class=\"footdiv\">"
+renderFootDiv :: Bool -> Maybe (String, String) -> Maybe (String, String) -> Text
+renderFootDiv isTop mPrev mNext = T.unlines
+    [ "<div class=\"footdiv\"" <> (if isTop then " style=\"margin-bottom:25px;\"" else "") <> ">"
     , "    <ul>"
-    ,       case mPrev of
-                Nothing -> "<li class=\"prev\"></li>"
-                Just p  -> "<li class=\"prev\"><a href=\"" <> T.pack p <> ".html\">Anterior</a></li>"
-    , "        <li class=\"toc\"><a href=\"index.html\">Sumário</a></li>"
-    ,       case mNext of
-                Nothing -> "<li class=\"next\"></li>"
-                Just n  -> "<li class=\"next\"><a href=\"" <> T.pack n <> ".html\">Próximo</a></li>"
+    , "        <li style=\"text-align:left\">"
+    ,           case mPrev of
+                    Nothing -> ""
+                    Just (f, t) -> "<a href=\"" <> T.pack f <> ".html\">Anterior (" <> T.pack t <> ")</a>"
+    , "        </li>"
+    , "        <li style=\"text-align:center\">"
+    , "            <a href=\"chapters.html\">Índice de capítulos</a>"
+    , "        </li>"
+    , "        <li style=\"text-align:right\">"
+    ,           case mNext of
+                    Nothing -> ""
+                    Just (f, t) -> "<a href=\"" <> T.pack f <> ".html\" class=\"nxtlink\">Próximo (" <> T.pack t <> ")</a>"
+    , "        </li>"
     , "    </ul>"
     , "</div>"
     ]
+
+-- ==========================================
+-- TOC GENERATION
+-- ==========================================
+
+generateTOC :: IO ()
+generateTOC = do
+    putStrLn "Gerando chapters.html (TOC)..."
+    let tocContent = T.unlines
+            [ "<h1>Índice de capítulos</h1>"
+            , "<ol class=\"chapters\" type=\"1\">"
+            , T.concat (map renderTocItem orderedChapters)
+            , "</ol>"
+            ]
+    let finalHtml = layout "introcontent" Nothing Nothing tocContent
+    TIO.writeFile (outputDir </> "chapters.html") finalHtml
+
+renderTocItem :: (String, String) -> Text
+renderTocItem (file, title) = 
+    "    <li><a href=\"" <> T.pack file <> ".html\">" <> T.pack title <> "</a></li>"
 
 -- ==========================================
 -- MAIN BUILD STEP
@@ -109,13 +158,13 @@ renderFootDiv mPrev mNext = T.unlines
 
 main :: IO ()
 main = do
-    putStrLn "--- Iniciando build simplificado ---"
+    putStrLn "--- Iniciando build de alta fidelidade ---"
     createDirectoryIfMissing True outputDir
     
     let total = length orderedChapters
     
-    forM_ (zip [0..] orderedChapters) $ \(i, name) -> do
-        putStrLn $ "Renderizando [" ++ show (i+1) ++ "/" ++ show total ++ "]: " ++ name
+    forM_ (zip [0..] orderedChapters) $ \(i, (name, chapterTitle)) -> do
+        putStrLn $ "Renderizando [" ++ show (i+1) ++ "/" ++ show total ++ "]: " ++ name ++ " (" ++ chapterTitle ++ ")"
         
         -- Lê MD da raiz
         content <- TIO.readFile (name ++ ".md")
@@ -124,13 +173,16 @@ main = do
         let mPrev = if i > 0 then Just (orderedChapters !! (i-1)) else Nothing
         let mNext = if i < total - 1 then Just (orderedChapters !! (i+1)) else Nothing
         
-        let finalHtml = layout mPrev mNext htmlBody
+        -- Capítulos no LYAH original usam "introcontent" como classe
+        let finalHtml = layout "introcontent" mPrev mNext htmlBody
         TIO.writeFile (outputDir </> name ++ ".html") finalHtml
     
-    -- index.html
-    let firstChapter = head orderedChapters
-    copyFile (outputDir </> firstChapter ++ ".html") (outputDir </> "index.html")
-    putStrLn $ "index.html criado a partir de " ++ firstChapter
+    -- Gerar TOC
+    generateTOC
+    
+    -- index.html redireciona para chapters.html ou é uma cópia da intro
+    copyFile (outputDir </> "chapters.html") (outputDir </> "index.html")
+    putStrLn "index.html criado a partir de chapters.html"
     
     -- Copiar Assets
     putStrLn "Copiando ativos..."
